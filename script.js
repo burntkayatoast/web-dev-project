ccfc95dd57fcd739fbd9201bdab5e8810e17e111
BeSuper123 <bugochukwu17@gmail.com>
2025-09-29 13:08:52 +0100

    added comments

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const app = express()
-app.set('view engine', 'ejs')
+const express = require('express') // includes express library
+const app = express() // creates an express app
+app.set('view engine', 'ejs') // sets the view engine to ejs
 
-app.use(express.static('views'))
+// Allow access to views folder
+app.use(express.static('views')) 
 
+//allows the extraction of an incoming request object and makes it available using req.body
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
+// home page
 app.get('/', (req, res) => {
   res.render("home")
   console.log('the home page is loaded')
 })
 
+// requirements to start the server
 app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function() {
   console.log("Server is running... YAYYYY!");
 })
