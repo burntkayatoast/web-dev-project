c369b6e5573c657eb13e7967091fa8b35f2fbcd3
BeSuper123 <bugochukwu17@gmail.com>
2025-09-29 13:00:35 +0100

    changed script.js and added ejs files

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+app.set('view engine', 'ejs')
+
+app.use(express.static('views'))
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("home")
+  console.log('the home page is loaded')
 })
 
-app.listen(port, () => {
-  console.log(`Hii, the app is listening on port ${port}`)
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function() {
+  console.log("Server is running... YAYYYY!");
 })
